7d58c1075da7aa932e8919c9db6dfce2952c864e
Romain Beaumont <romain.rom1@gmail.com>
2021-08-17 23:07:30 +0200

    Release 1.1.0

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,7 @@
+## 1.1.0
+
+* add image resizing mode
+
 ## 1.0.1
 
 * fixes
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         name = 'img2dataset',
         packages = find_packages(),
         include_package_data = True,
-        version = '1.0.1',
+        version = '1.1.0',
         license='MIT',
         description = 'Easily turn a set of image urls to an image dataset',
         long_description=long_description,
