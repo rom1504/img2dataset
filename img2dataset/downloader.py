fb80ec85d7570f4247683b71c610da6249655045
Ross Wightman <rwightman@gmail.com>
2021-12-17 16:08:17 -0800

    Enhance image resize: * add support for different downsample and upsample interpolation (area only interpolation for dowsample that is reasonable without extra filtering as Pillow does) * add ability to skip re-encode for jpg data with size that fits target already (reduce CPU and avoid quality loss for an unecessary encode) * pass jpeg encode quality through as arg, cv2 default is 95, lower is likely good enough for many uses (and far less impact than bad downsample choice) * ResizeMode enum instead of strings * use functional Albumentations for resize ops

--- img2dataset/downloader.py
+++ img2dataset/downloader.py
@@ -1,5 +1,6 @@
 """Img2dataset"""
 
+from enum import Enum
 from multiprocessing import Pool
 from multiprocessing.pool import ThreadPool
 from threading import Semaphore
@@ -22,9 +23,36 @@ import glob
 import logging
 import time
 import wandb
+import imghdr
 from .logging_utils import CappedCounter, SpeedLogger, StatusTableLogger
 
 logging.getLogger("exifread").setLevel(level=logging.CRITICAL)
+
+
+_INTER_STR_TO_CV2 = dict(
+    nearest=cv2.INTER_NEAREST,
+    linear=cv2.INTER_LINEAR,
+    bilinear=cv2.INTER_LINEAR,
+    cubic=cv2.INTER_CUBIC,
+    bicubic=cv2.INTER_CUBIC,
+    area=cv2.INTER_AREA,
+    lanczos=cv2.INTER_LANCZOS4,
+    lanczos4=cv2.INTER_LANCZOS4,
+)
+
+
+class ResizeMode(Enum):
+    no = 0
+    keep_ratio = 1
+    center_crop = 2
+    border = 3
+
+
+def inter_str_to_cv2(inter_str):
+    inter_str = inter_str.lower()
+    if inter_str not in _INTER_STR_TO_CV2:
+        raise Exception(f"Invalid option for interpolation: {inter_str}")
+    return _INTER_STR_TO_CV2[inter_str]
 
 
 def download_image(row, timeout):
@@ -49,33 +77,33 @@ def download_image(row, timeout):
 class Resizer:
     """Resize images"""
 
-    def __init__(self, image_size, resize_mode, resize_only_if_bigger):
+    def __init__(
+            self,
+            image_size,
+            resize_mode,
+            resize_only_if_bigger,
+            upscale_interpolation="lanczos",
+            downscale_interpolation="area",
+            encode_quality=95,
+            skip_reencode=False
+    ):
         self.image_size = image_size
+        if isinstance(resize_mode, str):
+            if resize_mode not in ResizeMode.__members__:
+                raise Exception(f"Invalid option for resize_mode: {resize_mode}")
+            resize_mode = ResizeMode[resize_mode]
         self.resize_mode = resize_mode
         self.resize_only_if_bigger = resize_only_if_bigger
-
-        if resize_mode not in ["no", "keep_ratio", "center_crop", "border"]:
-            raise Exception(f"Invalid option for resize_mode: {resize_mode}")
-
-        if resize_mode == "keep_ratio":
-            self.resize_tfm = A.SmallestMaxSize(image_size, interpolation=cv2.INTER_LANCZOS4)
-        elif resize_mode == "center_crop":
-            self.resize_tfm = A.Compose(
-                [A.SmallestMaxSize(image_size, interpolation=cv2.INTER_LANCZOS4), A.CenterCrop(image_size, image_size),]
-            )
-        elif resize_mode == "border":
-            self.resize_tfm = A.Compose(
-                [
-                    A.LongestMaxSize(image_size, interpolation=cv2.INTER_LANCZOS4),
-                    A.PadIfNeeded(image_size, image_size, border_mode=cv2.BORDER_CONSTANT, value=[255, 255, 255],),
-                ]
-            )
-        elif resize_mode == "no":
-            pass
+        self.upscale_interpolation = inter_str_to_cv2(upscale_interpolation)
+        self.downscale_interpolation = inter_str_to_cv2(downscale_interpolation)
+        self.encode_params = [int(cv2.IMWRITE_JPEG_QUALITY), encode_quality]
+        self.skip_reencode = skip_reencode
 
     def __call__(self, img_stream):
         try:
-            img = cv2.imdecode(np.frombuffer(img_stream.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+            encode_needed = imghdr.what(img_stream) != 'jpeg' if self.skip_reencode else True
+            img_buf = np.frombuffer(img_stream.read(), np.uint8)
+            img = cv2.imdecode(img_buf, cv2.IMREAD_UNCHANGED)
             if img is None:
                 raise Exception("Image decoding error")
             if len(img.shape) == 3 and img.shape[-1] == 4:
@@ -83,27 +111,31 @@ class Resizer:
                 alpha = img[:, :, 3, np.newaxis]
                 img = alpha / 255 * img[..., :3] + 255 - alpha
                 img = np.rint(img.clip(min=0, max=255)).astype(np.uint8)
+                encode_needed = True
             original_height, original_width = img.shape[:2]
 
             # resizing in following conditions
-            if self.resize_mode != "no" and (
-                not self.resize_only_if_bigger
-                or (
-                    self.resize_mode in ["keep_ratio", "center_crop"]
-                    # smallest side contained to image_size (largest cropped)
-                    and min(img.shape[:2]) > self.image_size
-                )
-                or (
-                    self.resize_mode == "border"
-                    # largest side contained to image_size
-                    and max(img.shape[:2]) > self.image_size
-                )
-            ):
-                img = self.resize_tfm(image=img)["image"]
-
+            if self.resize_mode in (ResizeMode.keep_ratio, ResizeMode.center_crop):
+                downscale = min(original_width, original_height) > self.image_size
+                if not self.resize_only_if_bigger or downscale:
+                    interpolation = self.downscale_interpolation if downscale else self.upscale_interpolation
+                    img = A.smallest_max_size(img, self.image_size, interpolation=interpolation)
+                    if self.resize_mode == ResizeMode.center_crop:
+                        img = A.center_crop(img, self.image_size, self.image_size)
+                    encode_needed = True
+            elif self.resize_mode == ResizeMode.border:
+                downscale = max(original_width, original_height) > self.image_size
+                if not self.resize_only_if_bigger or downscale:
+                    interpolation = self.downscale_interpolation if downscale else self.upscale_interpolation
+                    img = A.longest_max_size(img, self.image_size, interpolation=interpolation)
+                    img = A.pad(
+                        img, self.image_size, self.image_size, border_mode=cv2.BORDER_CONSTANT, value=[255, 255, 255])
+                    encode_needed = True
             height, width = img.shape[:2]
-            img_str = cv2.imencode(".jpg", img)[1].tobytes()
-            del img
+            if encode_needed:
+                img_str = cv2.imencode(".jpg", img, params=self.encode_params)[1].tobytes()
+            else:
+                img_str = img_buf.tobytes()
             return img_str, width, height, original_width, original_height, None
 
         except Exception as err:  # pylint: disable=broad-except
@@ -306,6 +338,10 @@ def download(
     processes_count: int = 1,
     resize_mode: str = "border",
     resize_only_if_bigger: bool = False,
+    upscale_interpolation: str = "lanczos",
+    downscale_interpolation: str = "area",
+    encode_quality: int = 95,
+    skip_reencode: bool = False,
     output_format: str = "files",
     input_format: str = "txt",
     url_col: str = "url",
@@ -374,6 +410,8 @@ def download(
                 column_list = column_list + ["url"]
             images_to_dl = df[column_list].to_records(index=False).tolist()
             del df
+        else:
+            assert False, f"Unexpected input format ({input_format})."
 
         sharded_images_to_dl = []
         number_samples = len(images_to_dl)
@@ -393,7 +431,15 @@ def download(
         elif output_format == "files":
             sample_writer_class = FilesSampleWriter  # type: ignore
 
-        resizer = Resizer(image_size=image_size, resize_mode=resize_mode, resize_only_if_bigger=resize_only_if_bigger,)
+        resizer = Resizer(
+            image_size=image_size,
+            resize_mode=resize_mode,
+            resize_only_if_bigger=resize_only_if_bigger,
+            upscale_interpolation=upscale_interpolation,
+            downscale_interpolation=downscale_interpolation,
+            encode_quality=encode_quality,
+            skip_reencode=skip_reencode,
+        )
 
         downloader = functools.partial(
             one_process_downloader,
