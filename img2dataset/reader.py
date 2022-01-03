5e40ba85789ff3a88bdd18106be209639a7634b3
Romain Beaumont <romain.rom1@gmail.com>
2022-01-03 03:24:09 +0100

    make reader memory efficient by using feather files (#83)
    save to sharded feather files then only read these small files

--- img2dataset/reader.py
+++ img2dataset/reader.py
@@ -3,6 +3,7 @@
 import glob
 import os
 import pandas as pd
+import tempfile
 import math
 
 
@@ -51,6 +52,48 @@ class Reader:
             else:
                 self.column_list = self.column_list + ["url"]
 
+    def _save_to_arrow(self, input_file, tmp_dir):
+        """Read the input file and save to arrow files in a temporary directory"""
+        if self.input_format == "txt":
+            with open(input_file, encoding="utf-8") as file:
+                df = pd.DataFrame([(url.rstrip(),) for url in file.readlines()], columns=self.column_list)
+        elif self.input_format in ["json", "csv", "tsv", "tsv.gz", "parquet"]:
+            if self.input_format == "json":
+                df = pd.read_json(input_file)
+            elif self.input_format == "csv":
+                df = pd.read_csv(input_file)
+            elif self.input_format in ("tsv", "tsv.gz"):
+                df = pd.read_table(input_file)
+            elif self.input_format == "parquet":
+                columns_to_read = [self.url_col]
+                if self.caption_col is not None:
+                    columns_to_read += [self.caption_col]
+                if self.save_additional_columns is not None:
+                    columns_to_read += self.save_additional_columns
+                df = pd.read_parquet(input_file, columns=columns_to_read)
+        else:
+            assert False, f"Unexpected input format ({self.input_format})."
+
+        df = df.rename(columns={self.caption_col: "caption", self.url_col: "url"})
+        df = df.where(pd.notnull(df), None)
+
+        number_samples = len(df)
+
+        number_shards = math.ceil(len(df) / self.number_sample_per_shard)
+
+        shards = []
+        for shard_id in range(number_shards):
+            begin_shard = shard_id * self.number_sample_per_shard
+            end_shard = min(number_samples, (1 + shard_id) * self.number_sample_per_shard)
+            df_shard = df[begin_shard:end_shard][self.column_list]
+            df_shard = df_shard.reset_index(drop=True)
+            file_name = tmp_dir + f"/{shard_id + self.start_shard_id}.feather"
+            df_shard.to_feather(file_name)
+            shards.append((shard_id, file_name))
+        del df
+
+        return shards
+
     def __iter__(self):
         """
         Iterate over shards, yield shards of size number_sample_per_shard or less for the last one
@@ -64,44 +107,19 @@ class Reader:
             )
             print("Loading the input file")
 
-            if self.input_format == "txt":
-                images_to_dl = []
-                with open(input_file, encoding="utf-8") as file:
-                    images_to_dl = [(url.rstrip(),) for url in file.readlines()]
-            elif self.input_format in ["json", "csv", "tsv", "tsv.gz", "parquet"]:
-                if self.input_format == "json":
-                    df = pd.read_json(input_file)
-                elif self.input_format == "csv":
-                    df = pd.read_csv(input_file)
-                elif self.input_format in ("tsv", "tsv.gz"):
-                    df = pd.read_table(input_file)
-                elif self.input_format == "parquet":
-                    columns_to_read = [self.url_col]
-                    if self.caption_col is not None:
-                        columns_to_read += [self.caption_col]
-                    if self.save_additional_columns is not None:
-                        columns_to_read += self.save_additional_columns
-                    df = pd.read_parquet(input_file, columns=columns_to_read)
-                df = df.rename(columns={self.caption_col: "caption", self.url_col: "url"})
-                df = df.where(pd.notnull(df), None)
-                images_to_dl = df[self.column_list].to_records(index=False).tolist()
-                del df
-            else:
-                assert False, f"Unexpected input format ({self.input_format})."
+            with tempfile.TemporaryDirectory("img2dataset") as tmp_dir:
+                shards = self._save_to_arrow(input_file, tmp_dir)
+                num_shard = 0
+                for num_shard, arrow_file in shards:
+                    df = pd.read_feather(arrow_file)
+                    images_to_dl = df[self.column_list].to_records(index=False).tolist()
+                    del df
 
-            number_samples = len(images_to_dl)
-            number_shards = math.ceil(number_samples / self.number_sample_per_shard)
-            print(
-                f"Splitting the {number_samples} samples in {number_shards}"
-                f"shards of size {self.number_sample_per_shard}"
-            )
-            for shard_id in range(number_shards):
-                begin_shard = shard_id * self.number_sample_per_shard
-                end_shard = min(number_samples, (1 + shard_id) * self.number_sample_per_shard)
-                yield (
-                    shard_id + self.start_shard_id,
-                    list(enumerate(images_to_dl[begin_shard:end_shard])),
-                )
-            self.start_shard_id += number_shards
-            del images_to_dl
-            print("Done sharding the input file")
+                    yield (
+                        num_shard + self.start_shard_id,
+                        list(enumerate(images_to_dl)),
+                    )
+
+                    num_shard += 1
+                self.start_shard_id += num_shard
+                del images_to_dl
