18b0e4c9a070bcda98171f573d1f3f4af2996641
Romain Beaumont <romain.rom1@gmail.com>
2022-02-03 22:36:26 +0100

    initialize logger fs in subprocess to avoid moving fs over a fork() (#117)

--- img2dataset/logger.py
+++ img2dataset/logger.py
@@ -190,7 +190,7 @@ class LoggerProcess(Process):
         super().__init__()
         self.log_interval = log_interval
         self.enable_wandb = enable_wandb
-        self.fs, self.output_path = fsspec.core.url_to_fs(output_folder)
+        self.output_folder = output_folder
         self.stats_files = set()
         self.wandb_project = wandb_project
         self.config_parameters = config_parameters
@@ -199,6 +199,8 @@ class LoggerProcess(Process):
 
     def run(self):
         """Run logger process"""
+
+        fs, output_path = fsspec.core.url_to_fs(self.output_folder)
 
         if self.enable_wandb:
             self.current_run = wandb.init(project=self.wandb_project, config=self.config_parameters, anonymous="allow")
@@ -225,7 +227,7 @@ class LoggerProcess(Process):
 
             try:
                 # read stats files
-                stats_files = self.fs.glob(self.output_path + "/*.json")
+                stats_files = fs.glob(output_path + "/*.json")
 
                 # get new stats files
                 new_stats_files = set(stats_files) - self.stats_files
@@ -236,7 +238,7 @@ class LoggerProcess(Process):
 
                 # read new stats files
                 for stats_file in new_stats_files:
-                    with self.fs.open(stats_file, "r") as f:
+                    with fs.open(stats_file, "r") as f:
                         try:
                             stats = json.load(f)
                             SpeedLogger("worker", enable_wandb=self.enable_wandb)(
