5026d79da00fa37988f84cec47c4e10c3abb8dbe
Romain Beaumont <romain.rom1@gmail.com>
2022-02-06 19:38:54 +0100

    hide opencv warnings (#128)
    also improve a little the reader prints

--- img2dataset/resizer.py
+++ img2dataset/resizer.py
@@ -5,6 +5,7 @@ import cv2
 import numpy as np
 from enum import Enum
 import imghdr
+import os
 
 _INTER_STR_TO_CV2 = dict(
     nearest=cv2.INTER_NEAREST,
@@ -23,6 +24,38 @@ class ResizeMode(Enum):
     keep_ratio = 1
     center_crop = 2
     border = 3
+
+
+# thanks https://stackoverflow.com/questions/11130156/suppress-stdout-stderr-print-from-python-functions
+class SuppressStdoutStderr:
+    """
+    A context manager for doing a "deep suppression" of stdout and stderr in
+    Python, i.e. will suppress all print, even if the print originates in a
+    compiled C/Fortran sub-function.
+       This will not suppress raised exceptions, since exceptions are printed
+    to stderr just before a script exits, and after the context manager has
+    exited (at least, I think that is why it lets exceptions through).
+
+    """
+
+    def __init__(self):
+        # Open a pair of null files
+        self.null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
+        # Save the actual stdout (1) and stderr (2) file descriptors.
+        self.save_fds = [os.dup(1), os.dup(2)]
+
+    def __enter__(self):
+        # Assign the null pointers to stdout and stderr.
+        os.dup2(self.null_fds[0], 1)
+        os.dup2(self.null_fds[1], 2)
+
+    def __exit__(self, *_):
+        # Re-assign the real stdout/stderr back to (1) and (2)
+        os.dup2(self.save_fds[0], 1)
+        os.dup2(self.save_fds[1], 2)
+        # Close all file descriptors
+        for fd in self.null_fds + self.save_fds:
+            os.close(fd)
 
 
 def inter_str_to_cv2(inter_str):
@@ -73,44 +106,49 @@ class Resizer:
         output: img_str, width, height, original_width, original_height, err
         """
         try:
-            cv2.setNumThreads(1)
-            encode_needed = imghdr.what(img_stream) != "jpeg" if self.skip_reencode else True
-            img_buf = np.frombuffer(img_stream.read(), np.uint8)
-            img = cv2.imdecode(img_buf, cv2.IMREAD_UNCHANGED)
-            if img is None:
-                raise Exception("Image decoding error")
-            if len(img.shape) == 3 and img.shape[-1] == 4:
-                # alpha matting with white background
-                alpha = img[:, :, 3, np.newaxis]
-                img = alpha / 255 * img[..., :3] + 255 - alpha
-                img = np.rint(img.clip(min=0, max=255)).astype(np.uint8)
-                encode_needed = True
-            original_height, original_width = img.shape[:2]
+            with SuppressStdoutStderr():
+                cv2.setNumThreads(1)
+                encode_needed = imghdr.what(img_stream) != "jpeg" if self.skip_reencode else True
+                img_buf = np.frombuffer(img_stream.read(), np.uint8)
+                img = cv2.imdecode(img_buf, cv2.IMREAD_UNCHANGED)
+                if img is None:
+                    raise Exception("Image decoding error")
+                if len(img.shape) == 3 and img.shape[-1] == 4:
+                    # alpha matting with white background
+                    alpha = img[:, :, 3, np.newaxis]
+                    img = alpha / 255 * img[..., :3] + 255 - alpha
+                    img = np.rint(img.clip(min=0, max=255)).astype(np.uint8)
+                    encode_needed = True
+                original_height, original_width = img.shape[:2]
 
-            # resizing in following conditions
-            if self.resize_mode in (ResizeMode.keep_ratio, ResizeMode.center_crop):
-                downscale = min(original_width, original_height) > self.image_size
-                if not self.resize_only_if_bigger or downscale:
-                    interpolation = self.downscale_interpolation if downscale else self.upscale_interpolation
-                    img = A.smallest_max_size(img, self.image_size, interpolation=interpolation)
-                    if self.resize_mode == ResizeMode.center_crop:
-                        img = A.center_crop(img, self.image_size, self.image_size)
-                    encode_needed = True
-            elif self.resize_mode == ResizeMode.border:
-                downscale = max(original_width, original_height) > self.image_size
-                if not self.resize_only_if_bigger or downscale:
-                    interpolation = self.downscale_interpolation if downscale else self.upscale_interpolation
-                    img = A.longest_max_size(img, self.image_size, interpolation=interpolation)
-                    img = A.pad(
-                        img, self.image_size, self.image_size, border_mode=cv2.BORDER_CONSTANT, value=[255, 255, 255]
-                    )
-                    encode_needed = True
-            height, width = img.shape[:2]
-            if encode_needed:
-                img_str = cv2.imencode(".jpg", img, params=self.encode_params)[1].tobytes()
-            else:
-                img_str = img_buf.tobytes()
-            return img_str, width, height, original_width, original_height, None
+                # resizing in following conditions
+                if self.resize_mode in (ResizeMode.keep_ratio, ResizeMode.center_crop):
+                    downscale = min(original_width, original_height) > self.image_size
+                    if not self.resize_only_if_bigger or downscale:
+                        interpolation = self.downscale_interpolation if downscale else self.upscale_interpolation
+                        img = A.smallest_max_size(img, self.image_size, interpolation=interpolation)
+                        if self.resize_mode == ResizeMode.center_crop:
+                            img = A.center_crop(img, self.image_size, self.image_size)
+                        encode_needed = True
+                elif self.resize_mode == ResizeMode.border:
+                    downscale = max(original_width, original_height) > self.image_size
+                    if not self.resize_only_if_bigger or downscale:
+                        interpolation = self.downscale_interpolation if downscale else self.upscale_interpolation
+                        img = A.longest_max_size(img, self.image_size, interpolation=interpolation)
+                        img = A.pad(
+                            img,
+                            self.image_size,
+                            self.image_size,
+                            border_mode=cv2.BORDER_CONSTANT,
+                            value=[255, 255, 255],
+                        )
+                        encode_needed = True
+                height, width = img.shape[:2]
+                if encode_needed:
+                    img_str = cv2.imencode(".jpg", img, params=self.encode_params)[1].tobytes()
+                else:
+                    img_str = img_buf.tobytes()
+                return img_str, width, height, original_width, original_height, None
 
         except Exception as err:  # pylint: disable=broad-except
             return None, None, None, None, None, str(err)
