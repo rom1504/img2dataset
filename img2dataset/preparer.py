ef78c437ff9b942cc7c82e79d5b61fdc5d6f8806
Boris Dayma <boris.dayma@gmail.com>
2022-03-06 06:24:25 +0000

    tests: add types-requests

---
+++ img2dataset/preparer.py
@@ -0,0 +1,12 @@
+# this tool will automatically prepare the feather files
+# these files can be read by the worker
+
+# it can be used to prepare all files before starting
+# or run in parallel while workers start processing some files
+
+# also possible to run it multiple times
+# once every so often
+
+
+# the worker will take a subset of shards and get them
+
