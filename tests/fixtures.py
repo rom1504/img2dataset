5c6df02cb6324c079ffeb3f013e39a3e4fefe5c9
Romain Beaumont <romain.rom1@gmail.com>
2022-04-04 03:00:09 +0200

    none -> "" in fixture
    probably not the best idea
    see how to do this in the reader with pyarrow instead

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -15,7 +15,7 @@ def setup_fixtures(count=5):
     for i in range(count):
         item = random.randint(0, len(image_paths) - 1)
         test_list.append(
-            (f"caption {i}" if i != 0 else None, image_paths[item].replace(test_folder, f"http://localhost:{port}"))
+            (f"caption {i}" if i != 0 else "", image_paths[item].replace(test_folder, f"http://localhost:{port}"))
         )
     test_list = test_list[:count]
 
