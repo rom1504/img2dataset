c0f14c9020003483f9b30b960317187f9a6c6b97
Remi Delbouys <remidbs@users.noreply.github.com>
2022-06-24 12:04:11 +0200

    Fix error message (#178)

--- img2dataset/reader.py
+++ img2dataset/reader.py
@@ -49,7 +49,7 @@ class Reader:
         if fs.isdir(url_path):
             self.input_files = sorted(fs.glob(url_path + "/*." + input_format))
             if len(self.input_files) == 0:
-                raise Exception("No file found at path {url_path} with extension {input_format}")
+                raise Exception(f"No file found at path {url_path} with extension {input_format}")
         else:
             self.input_files = [url_path]
 
