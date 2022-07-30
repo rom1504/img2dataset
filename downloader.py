ba18254e51db5f4488ebf785baa61911b413ef97
nicolai256 <68881396+nicolai256@users.noreply.github.com>
2022-07-30 17:36:19 +0200

    Add files via upload

---
+++ downloader.py
@@ -0,0 +1,285 @@
+"""the downloader module handles the downloading"""
+
+from multiprocessing.pool import ThreadPool
+from threading import Semaphore
+import urllib.request
+import io
+import math
+import exifread
+import json
+import time
+import hashlib
+import pyarrow as pa
+import traceback
+import sys
+import fsspec
+from .logger import CappedCounter
+from .logger import write_stats
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
+def download_image(row, timeout):
+    """Download an image with urllib"""
+    key, url = row
+    img_stream = None
+    try:
+        request = urllib.request.Request(
+            url,
+            data=None,
+            headers={"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0"},
+        )
+        with urllib.request.urlopen(request, timeout=timeout) as r:
+            img_stream = io.BytesIO(r.read())
+        return key, img_stream, None
+    except Exception as err:  # pylint: disable=broad-except
+        if img_stream is not None:
+            img_stream.close()
+        return key, None, str(err)
+
+
+def download_image_with_retry(row, timeout, retries):
+    for _ in range(retries + 1):
+        key, img_stream, err = download_image(row, timeout)
+        if img_stream is not None:
+            return key, img_stream, err
+    return key, None, err
+
+
+def compute_key(key, shard_id, oom_sample_per_shard, oom_shard_count):
+    true_key = (10**oom_sample_per_shard) * shard_id + key
+    key_format = oom_sample_per_shard + oom_shard_count
+    str_key = "{true_key:0{key_format}d}".format(  # pylint: disable=consider-using-f-string
+        key_format=key_format, true_key=true_key
+    )
+    return str_key
+
+
+class Downloader:
+    """The downloader class gets calls with shards, download them then call the writer to write them down"""
+
+    def __init__(
+        self,
+        sample_writer_class,
+        resizer,
+        thread_count,
+        save_caption,
+        extract_exif,
+        output_folder,
+        column_list,
+        timeout,
+        number_sample_per_shard,
+        oom_shard_count,
+        compute_md5,
+        encode_format,
+        retries,
+    ) -> None:
+        self.sample_writer_class = sample_writer_class
+        self.resizer = resizer
+        self.thread_count = thread_count
+        self.save_caption = save_caption
+        self.extract_exif = extract_exif
+        self.output_folder = output_folder
+        self.column_list = column_list
+        self.timeout = timeout
+        self.number_sample_per_shard = number_sample_per_shard
+        self.oom_shard_count = oom_shard_count
+        self.compute_md5 = compute_md5
+        self.encode_format = encode_format
+        self.retries = retries
+
+    def __call__(
+        self,
+        row,
+    ):
+        try:
+            self.download_shard(row)
+            return (True, row)
+        except Exception as err:  # pylint: disable=broad-except
+            traceback.print_exc()
+            print(f"shard {row[0]} failed with error {err}")
+            return (False, row)
+
+    def download_shard(
+        self,
+        row,
+    ):
+        """Function to start an image downloading in one process"""
+
+        shard_id, shard_file = row
+        start_time = time.time()
+
+        fs, shard_path = fsspec.core.url_to_fs(shard_file)
+        with fs.open(shard_path, "rb") as f:
+            df = pa.ipc.open_file(f).read_all()
+        schema = df.schema
+        schema = (
+            schema.append(pa.field("key", pa.string()))
+            .append(pa.field("status", pa.string()))
+            .append(pa.field("error_message", pa.string()))
+            .append(pa.field("width", pa.int32()))
+            .append(pa.field("height", pa.int32()))
+            .append(pa.field("original_width", pa.int32()))
+            .append(pa.field("original_height", pa.int32()))
+        )
+        if self.extract_exif:
+            schema = schema.append(pa.field("exif", pa.string()))
+
+        if self.compute_md5:
+            schema = schema.append(pa.field("md5", pa.string()))
+
+        pydict = df.select(self.column_list).to_pydict()
+        shard_to_dl = list(enumerate(zip(*(pydict[col] for col in self.column_list))))
+        del pydict
+        del df
+
+        status_dict = CappedCounter()
+
+        count = len(shard_to_dl)
+        successes = 0
+        failed_to_download = 0
+        failed_to_resize = 0
+        url_indice = self.column_list.index("url")
+        caption_indice = self.column_list.index("caption") if "caption" in self.column_list else None
+        key_url_list = [(key, x[url_indice]) for key, x in shard_to_dl]
+
+        # this prevents an accumulation of more than twice the number of threads in sample ready to resize
+        # limit the memory usage
+        semaphore = Semaphore(self.thread_count * 2)
+
+        def data_generator():
+            for e in key_url_list:
+                semaphore.acquire()  # pylint: disable=consider-using-with
+                yield e
+
+        loader = data_generator()
+
+        # give schema to writer
+        sample_writer = self.sample_writer_class(
+            shard_id,
+            self.output_folder,
+            self.save_caption,
+            self.oom_shard_count,
+            schema,
+            self.encode_format,
+        )
+        oom_sample_per_shard = math.ceil(math.log10(self.number_sample_per_shard))
+        with ThreadPool(self.thread_count) as thread_pool:
+            for key, img_stream, error_message in thread_pool.imap_unordered(
+                lambda x: download_image_with_retry(x, timeout=self.timeout, retries=self.retries),
+                loader,
+            ):
+                try:
+                    _, sample_data = shard_to_dl[key]
+                    str_key = compute_key(key, shard_id, oom_sample_per_shard, self.oom_shard_count)
+                    meta = {
+                        **{self.column_list[i]: sample_data[i] for i in range(len(self.column_list))},
+                        "key": str_key,
+                        "status": None,
+                        "error_message": error_message,
+                        "width": None,
+                        "height": None,
+                        "original_width": None,
+                        "original_height": None,
+                    }
+                    if self.extract_exif:
+                        meta["exif"] = None
+                    if self.compute_md5:
+                        meta["md5"] = None
+                    if error_message is not None:
+                        failed_to_download += 1
+                        status = "failed_to_download"
+                        status_dict.increment(error_message)
+                        meta["status"] = status
+                        sample_writer.write(
+                            None,
+                            str_key,
+                            sample_data[caption_indice] if caption_indice is not None else None,
+                            meta,
+                        )
+                        semaphore.release()
+                        continue
+                    img_stream.seek(0)
+                    (
+                        img,
+                        width,
+                        height,
+                        original_width,
+                        original_height,
+                        error_message,
+                    ) = self.resizer(img_stream)
+                    if error_message is not None:
+                        failed_to_resize += 1
+                        status = "failed_to_resize"
+                        status_dict.increment(error_message)
+                        meta["status"] = status
+                        meta["error_message"] = error_message
+                        sample_writer.write(
+                            None,
+                            str_key,
+                            sample_data[caption_indice] if caption_indice is not None else None,
+                            meta,
+                        )
+                        img_stream.close()
+                        del img_stream
+                        semaphore.release()
+                        continue
+                    successes += 1
+                    status = "success"
+                    status_dict.increment(status)
+
+                    if self.extract_exif:
+                        try:
+                            img_stream.seek(0)
+                            exif = json.dumps(
+                                {
+                                    k: str(v).strip()
+                                    for k, v in exifread.process_file(img_stream, details=False).items()
+                                    if v is not None
+                                }
+                            )
+                        except Exception as _:  # pylint: disable=broad-except
+                            exif = None
+                        meta["exif"] = exif
+
+                    if self.compute_md5:
+                        img_stream.seek(0)
+                        meta["md5"] = hashlib.md5(img_stream.read()).hexdigest()
+
+                    meta["status"] = status
+                    meta["width"] = width
+                    meta["height"] = height
+                    meta["original_width"] = original_width
+                    meta["original_height"] = original_height
+                    img_stream.close()
+                    del img_stream
+
+                    sample_writer.write(
+                        img,
+                        str_key,
+                        sample_data[caption_indice] if caption_indice is not None else None,
+                        meta,
+                    )
+                except Exception as err:  # pylint: disable=broad-except
+                    traceback.print_exc()
+                    print(f"Sample {key} failed to download: {err}")
+                semaphore.release()
+
+            sample_writer.close()
+            thread_pool.terminate()
+            thread_pool.join()
+            del thread_pool
+
+        end_time = time.time()
+        write_stats(
+            self.output_folder,
+            shard_id,
+            count,
+            successes,
+            failed_to_download,
+            failed_to_resize,
+            start_time,
+            end_time,
+            status_dict,
+            self.oom_shard_count,
+        )
+        fs.rm(shard_path)
---
+++ writer.py
@@ -0,0 +1,292 @@
+""""writer module handle writing the images to disk"""
+
+import webdataset as wds
+import json
+import pyarrow.parquet as pq
+import pyarrow as pa
+import fsspec
+import os
+import numpy as np
+import sys
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
+class BufferedParquetWriter:
+    """Write samples to parquet files incrementally with a buffer"""
+
+    def __init__(self, output_file, schema, buffer_size=100):
+        self.buffer_size = buffer_size
+        self.schema = schema
+        self._initiatlize_buffer()
+        fs, output_path = fsspec.core.url_to_fs(output_file)
+
+        self.output_fd = fs.open(output_path, "wb")
+        self.parquet_writer = pq.ParquetWriter(self.output_fd, schema)
+
+    def _initiatlize_buffer(self):
+        self.current_buffer_size = 0
+        self.buffer = {k: [] for k in self.schema.names}
+
+    def _add_sample_to_buffer(self, sample):
+        for k in self.schema.names:
+            self.buffer[k].append(sample[k])
+        self.current_buffer_size += 1
+
+    def write(self, sample):
+        if self.current_buffer_size >= self.buffer_size:
+            self.flush()
+        self._add_sample_to_buffer(sample)
+
+    def flush(self):
+        """Write the buffer to disk"""
+        if self.current_buffer_size == 0:
+            return
+
+        df = pa.Table.from_pydict(self.buffer, self.schema)
+        self.parquet_writer.write_table(df)
+        self._initiatlize_buffer()
+
+    def close(self):
+        self.flush()
+        if self.parquet_writer is not None:
+            self.parquet_writer.close()
+            self.parquet_writer = None
+            self.output_fd.close()
+
+
+class ParquetSampleWriter:
+    """ParquetSampleWriter is a image+caption writer to parquet"""
+
+    def __init__(
+        self,
+        shard_id,
+        output_folder,
+        save_caption,
+        oom_shard_count,
+        schema,
+        encode_format,
+    ):
+        self.oom_shard_count = oom_shard_count
+        self.encode_format = encode_format
+        schema = schema.append(pa.field(encode_format, pa.binary()))
+        shard_name = "{shard_id:0{oom_shard_count}d}".format(  # pylint: disable=consider-using-f-string
+            shard_id=shard_id, oom_shard_count=oom_shard_count
+        )
+        output_file = f"{output_folder}/{shard_name}.parquet"
+        self.buffered_parquet_writer = BufferedParquetWriter(output_file, schema, 100)
+        self.save_caption = save_caption
+
+    def write(self, img_str, key, caption, meta):
+        """Keep sample in memory then write to disk when close() is called"""
+        if img_str is not None:
+            sample = {"key": key, self.encode_format: img_str}
+            if self.save_caption:
+                sample["txt"] = str(caption) if caption is not None else ""
+        else:
+            sample = {"key": key, self.encode_format: None}
+            if self.save_caption:
+                sample["txt"] = None
+        sample.update(meta)
+        self.buffered_parquet_writer.write(sample)
+
+    def close(self):
+        self.buffered_parquet_writer.close()
+
+
+class WebDatasetSampleWriter:
+    """WebDatasetSampleWriter is a image+caption writer to webdataset"""
+
+    def __init__(
+        self,
+        shard_id,
+        output_folder,
+        save_caption,
+        oom_shard_count,
+        schema,
+        encode_format,
+    ):
+        self.oom_shard_count = oom_shard_count
+        shard_name = "{shard_id:0{oom_shard_count}d}".format(  # pylint: disable=consider-using-f-string
+            shard_id=shard_id, oom_shard_count=oom_shard_count
+        )
+        self.shard_id = shard_id
+        fs, output_path = fsspec.core.url_to_fs(output_folder)
+        self.tar_fd = fs.open(f"{output_path}/{shard_name}.tar", "wb")
+        self.tarwriter = wds.TarWriter(self.tar_fd)
+        self.save_caption = save_caption
+        self.buffered_parquet_writer = BufferedParquetWriter(output_folder + "/" + shard_name + ".parquet", schema, 100)
+        self.encode_format = encode_format
+
+    def write(self, img_str, key, caption, meta):
+        """write sample to tars"""
+        if img_str is not None:
+            sample = {"__key__": key, self.encode_format: img_str}
+            if self.save_caption:
+                sample["txt"] = str(caption) if caption is not None else ""
+            # some meta data may not be JSON serializable
+            for k, v in meta.items():
+                if isinstance(v, np.ndarray):
+                    meta[k] = v.tolist()
+            sample["json"] = json.dumps(meta, indent=4)
+            self.tarwriter.write(sample)
+        self.buffered_parquet_writer.write(meta)
+
+    def close(self):
+        self.buffered_parquet_writer.close()
+        self.tarwriter.close()
+        self.tar_fd.close()
+
+
+class TFRecordSampleWriter:
+    """TFRecordSampleWriter is a image+caption writer to TFRecord"""
+
+    def __init__(
+        self,
+        shard_id,
+        output_folder,
+        save_caption,
+        oom_shard_count,
+        schema,
+        encode_format,
+    ):
+        try:
+            os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+            import tensorflow_io as _  # pylint: disable=import-outside-toplevel
+            from tensorflow.python.training.training import (  # pylint: disable=import-outside-toplevel
+                BytesList,
+                Int64List,
+                FloatList,
+                Example,
+                Features,
+                Feature,
+            )
+            from tensorflow.python.lib.io.tf_record import TFRecordWriter  # pylint: disable=import-outside-toplevel
+
+            self._BytesList = BytesList  # pylint: disable=invalid-name
+            self._Int64List = Int64List  # pylint: disable=invalid-name
+            self._FloatList = FloatList  # pylint: disable=invalid-name
+            self._Example = Example  # pylint: disable=invalid-name
+            self._Features = Features  # pylint: disable=invalid-name
+            self._Feature = Feature  # pylint: disable=invalid-name
+        except ImportError as e:
+            raise ModuleNotFoundError(
+                "tfrecords require tensorflow and tensorflow_io to be installed."
+                "Run `pip install tensorflow tensorflow_io`."
+            ) from e
+
+        self.oom_shard_count = oom_shard_count
+        shard_name = "{shard_id:0{oom_shard_count}d}".format(  # pylint: disable=consider-using-f-string
+            shard_id=shard_id, oom_shard_count=oom_shard_count
+        )
+        self.shard_id = shard_id
+        self.tf_writer = TFRecordWriter(f"{output_folder}/{shard_name}.tfrecord")
+        self.save_caption = save_caption
+        self.buffered_parquet_writer = BufferedParquetWriter(output_folder + "/" + shard_name + ".parquet", schema, 100)
+        self.encode_format = encode_format
+
+    def write(self, img_str, key, caption, meta):
+        """Write a sample using tfrecord writer"""
+        if img_str is not None:
+            sample = {
+                "key": self._bytes_feature(key.encode()),
+                self.encode_format: self._bytes_feature(img_str),
+            }
+            if self.save_caption:
+                sample["txt"] = self._bytes_feature(str(caption) if caption is not None else "")
+            for k, v in meta.items():
+                sample[k] = self._feature(v)
+            tf_example = self._Example(features=self._Features(feature=sample))
+            self.tf_writer.write(tf_example.SerializeToString())
+        self.buffered_parquet_writer.write(meta)
+
+    def close(self):
+        self.buffered_parquet_writer.close()
+        self.tf_writer.close()
+
+    def _feature(self, value):
+        """Convert to proper feature type"""
+        if isinstance(value, int):
+            return self._int64_feature(value)
+        elif isinstance(value, float):
+            return self._float_feature(value)
+        else:
+            return self._bytes_feature(value)
+
+    def _bytes_feature(self, value):
+        """Returns a bytes_list from a string / byte."""
+        if value is None:
+            value = ""
+        if isinstance(value, str):
+            value = value.encode()
+        return self._Feature(bytes_list=self._BytesList(value=[value]))
+
+    def _float_feature(self, value):
+        """Returns a float_list from a float / double."""
+        return self._Feature(float_list=self._FloatList(value=[value]))
+
+    def _int64_feature(self, value):
+        """Returns an int64_list from a bool / enum / int / uint."""
+        return self._Feature(int64_list=self._Int64List(value=[value]))
+
+
+class FilesSampleWriter:
+    """FilesSampleWriter is a caption+image writer to files"""
+
+    def __init__(
+        self,
+        shard_id,
+        output_folder,
+        save_caption,
+        oom_shard_count,
+        schema,
+        encode_format,
+    ):
+        self.oom_shard_count = oom_shard_count
+        shard_name = "{shard_id:0{oom_shard_count}d}".format(  # pylint: disable=consider-using-f-string
+            shard_id=shard_id, oom_shard_count=oom_shard_count
+        )
+        self.shard_id = shard_id
+        self.fs, self.subfolder = fsspec.core.url_to_fs(f"{output_folder}/{shard_name}")
+        if not self.fs.exists(self.subfolder):
+            self.fs.mkdir(self.subfolder)
+        self.save_caption = save_caption
+        self.buffered_parquet_writer = BufferedParquetWriter(output_folder + "/" + shard_name + ".parquet", schema, 100)
+        self.encode_format = encode_format
+
+    def write(self, img_str, key, caption, meta):
+        """Write sample to disk"""
+        if img_str is not None:
+            filename = f"{self.subfolder}/{key}.{self.encode_format}"
+            with self.fs.open(filename, "wb") as f:
+                f.write(img_str)
+            if self.save_caption:
+                caption = str(caption) if caption is not None else ""
+                caption_filename = f"{self.subfolder}/{key}.txt"
+                with self.fs.open(caption_filename, "w", encoding="utf-8") as f:
+                    f.write(str(caption))
+
+            # some meta data may not be JSON serializable
+            for k, v in meta.items():
+                if isinstance(v, np.ndarray):
+                    meta[k] = v.tolist()
+            j = json.dumps(meta, indent=4)
+            meta_filename = f"{self.subfolder}/{key}.json"
+            with self.fs.open(meta_filename, "w", encoding="utf-8") as f:
+                f.write(j)
+        self.buffered_parquet_writer.write(meta)
+
+    def close(self):
+        self.buffered_parquet_writer.close()
+
+
+class DummySampleWriter:
+    """Does not write"""
+
+    def __init__(self, shard_id, output_folder, save_caption, oom_shard_count, schema, encode_format):
+        pass
+
+    def write(self, img_str, key, caption, meta):
+        pass
+
+    def close(self):
+        pass
