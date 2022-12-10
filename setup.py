1d30868e1b8d131a3c958b1f31c1527c8810df97
Romain Beaumont <romain.rom1@gmail.com>
2022-12-10 16:19:58 +0100

    Release 1.36.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         name="img2dataset",
         packages=find_packages(),
         include_package_data=True,
-        version="1.35.0",
+        version="1.36.0",
         license="MIT",
         description="Easily turn a set of image urls to an image dataset",
         long_description=long_description,
