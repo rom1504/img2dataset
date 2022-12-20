bcb0ad9e09edd6efd47942872db25929bdccf9d3
Georgios Smyrnis <gsmyrnis@utexas.edu>
2022-12-20 02:58:40 -0600

    Bugfix in resizer logic.

--- img2dataset/resizer.py
+++ img2dataset/resizer.py
@@ -167,15 +167,16 @@ class Resizer:
                 if max(original_height, original_width) / min(original_height, original_width) > self.max_aspect_ratio:
                     return None, None, None, None, None, "aspect ratio too large"
 
-                # check if resizer was defined
-                maybe_blur_later = False
+                # check if resizer was defined during init if needed
                 if blurring_bbox_list is not None and self.blurrer is None:
                     return None, None, None, None, None, "blurrer not defined"
+
+                # Flag to check if blurring is still needed.
+                maybe_blur_still_needed = True
 
                 # resizing in following conditions
                 if self.resize_mode in (ResizeMode.keep_ratio, ResizeMode.center_crop):
                     downscale = min(original_width, original_height) > self.image_size
-                    maybe_blur_later = self.resize_only_if_bigger and not downscale
                     if not self.resize_only_if_bigger or downscale:
                         interpolation = self.downscale_interpolation if downscale else self.upscale_interpolation
                         img = A.smallest_max_size(img, self.image_size, interpolation=interpolation)
@@ -184,9 +185,9 @@ class Resizer:
                         if self.resize_mode == ResizeMode.center_crop:
                             img = A.center_crop(img, self.image_size, self.image_size)
                         encode_needed = True
+                        maybe_blur_still_needed = False
                 elif self.resize_mode in (ResizeMode.border, ResizeMode.keep_ratio_largest):
                     downscale = max(original_width, original_height) > self.image_size
-                    maybe_blur_later = self.resize_only_if_bigger and not downscale
                     if not self.resize_only_if_bigger or downscale:
                         interpolation = self.downscale_interpolation if downscale else self.upscale_interpolation
                         img = A.longest_max_size(img, self.image_size, interpolation=interpolation)
@@ -201,9 +202,10 @@ class Resizer:
                                 value=[255, 255, 255],
                             )
                         encode_needed = True
+                        maybe_blur_still_needed = False
 
                 # blur parts of the image if needed
-                if maybe_blur_later and blurring_bbox_list is not None and self.blurrer is not None:
+                if maybe_blur_still_needed and blurring_bbox_list is not None and self.blurrer is not None:
                     img = self.blurrer(img=img, bbox_list=blurring_bbox_list)
 
                 height, width = img.shape[:2]
