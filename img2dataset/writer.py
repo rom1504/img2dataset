abd4b02b8a93cc47a577401e8fd31fd3d21a5779
Vaishaal Shankar <vaishaal_shankar@apple.com>
2023-01-26 22:43:49 -0800

    add block size for fs open

--- img2dataset/writer.py
+++ img2dataset/writer.py
@@ -18,8 +18,8 @@ class BufferedParquetWriter:
         self.schema = schema
         self._initiatlize_buffer()
         fs, output_path = fsspec.core.url_to_fs(output_file)
-
-        self.output_fd = fs.open(output_path, "wb")
+        # testing for S3 
+        self.output_fd = fs.open(output_path, "wb", blocksize=200000000)
         self.parquet_writer = pq.ParquetWriter(self.output_fd, schema)
 
     def _initiatlize_buffer(self):
